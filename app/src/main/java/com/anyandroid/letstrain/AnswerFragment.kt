4c6cf72102032781e3daa1c69009786bd75b8dbe
Haithem Attab <lorenzomedici2001@gmail.com>
2023-05-15 23:50:20 +0200

    Expert-system

--- app/src/main/java/com/anyandroid/letstrain/AnswerFragment.kt
+++ app/src/main/java/com/anyandroid/letstrain/AnswerFragment.kt
@@ -1,51 +1,18 @@
-package com.anyandroid.letstrain
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.TextView
 import com.anyandroid.letstrain.databinding.FragmentAnswerBinding
 import java.io.Serializable
+import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.abs
 
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AnswerFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AnswerFragment : Fragment() {
-    lateinit var binding: FragmentAnswerBinding
+    private lateinit var binding: FragmentAnswerBinding
     private var trainingMap: MutableMap<String, String>? = null
-
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        binding= FragmentAnswerBinding.inflate(inflater, container, false)
-
-        // Retrieve trainingMap from arguments
-        trainingMap = arguments?.getSerializable("trainingMap") as? MutableMap<String, String>
-        binding.textView.text = trainingMap?.map { (key, value) -> "$key = $value" }?.joinToString(", ")
-
-        // Display the data in a Toast
-        val toastText = trainingMap?.map { (key, value) -> "$key = $value" }?.joinToString(", ")
-        toastText?.let {
-            Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
-        }
-        return binding.root
-    }
-
-
-
-
-
-
 
     companion object {
         @JvmStatic
@@ -55,5 +22,71 @@ class AnswerFragment : Fragment() {
                     putSerializable("trainingMap", trainingMap as Serializable)
                 }
             }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAnswerBinding.inflate(inflater, container, false)
+
+        // Retrieve trainingMap from arguments
+        trainingMap = arguments?.getSerializable("trainingMap") as? MutableMap<String, String>
+
+        val muscleGroups = findMuscleGroups(trainingMap!!)
+
+        val muscleGroupsString = muscleGroups.joinToString(", ")
+
+        binding.textView.text = muscleGroupsString
+
+        return binding.root
+    }
+
+    private fun findMuscleGroups(map: Map<String, String>): List<String> {
+        val currentDate = Calendar.getInstance().time
+        val dateFormatter = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+
+        val muscleGroups: MutableList<String> = mutableListOf()
+
+        val allowedMuscleGroups = setOf("Legs", "Chest", "Back", "Arms")
+
+        val latestMuscleGroupDates = mutableMapOf<String, Date>()
+
+        for ((dateString, muscleGroup) in map) {
+            val date = dateFormatter.parse(dateString)
+
+            if (muscleGroup !in allowedMuscleGroups)
+                continue
+
+            if (latestMuscleGroupDates.containsKey(muscleGroup)) {
+                val latestDate = latestMuscleGroupDates[muscleGroup]
+                if (date.after(latestDate)) {
+                    latestMuscleGroupDates[muscleGroup] = date
+                }
+            } else {
+                latestMuscleGroupDates[muscleGroup] = date
+            }
+        }
+
+        for ((muscleGroup, latestDate) in latestMuscleGroupDates) {
+            val difference = abs(currentDate.time - latestDate.time) / (24 * 60 * 60 * 1000)
+            val targetDifference = getTargetDifference(muscleGroup)
+
+            if (difference >= targetDifference) {
+                muscleGroups.add(muscleGroup)
+            }
+        }
+
+        return if (muscleGroups.isNotEmpty()) muscleGroups else listOf("Rest")
+    }
+
+    private fun getTargetDifference(muscleGroup: String): Int {
+        return when (muscleGroup) {
+            "Legs" -> 4
+            "Chest" -> 3
+            "Back" -> 3
+            "Arms" -> 2
+            else -> 0
+        }
     }
 }
